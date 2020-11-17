ec254dec2200629602fa53a3949c11f038cc6e08
eonr <sasikiran.bh@gmail.com>
2020-11-18 02:26:16 +0530

    Fixed server code

--- server.py
+++ server.py
@@ -5,28 +5,23 @@ import io
 import time
 import PIL.Image as Image
 
-def merge_img(file_pieces):
+NUM_THREADS = 8
+def merge_img():
+    global recieved_file_pieces
     while True:
-        time.sleep(5)
-        final_file = b''
-        keys = list(file_pieces.keys())
-        keys.sort()
-        for key in keys:
-            v = file_pieces[key]
-            print(f"key = {key} data = {len(v)}")
-            final_file += v
-        print(len(final_file))
-        image = Image.open(io.BytesIO(final_file))
-        image.save('recieved.jpg')
-        exit()
-        # if len(file_pieces) == NUM_THREADS:
-        #     final_file = b''
-        #     for k, v in file_pieces.items():
-        #         final_file += v
-        #     print(len(final_file))
-        #     image = Image.open(io.BytesIO(final_file))
-        #     image.save('recieved.JPG')
-        #     exit()
+        if len(recieved_file_pieces)==NUM_THREADS:
+            final_file = b''
+            keys = list(recieved_file_pieces.keys())
+            keys.sort()
+            for key in keys:
+                v = recieved_file_pieces[key]
+                print(f"key = {key} data = {len(v)}")
+                final_file += v
+            print(len(final_file))
+            image = Image.open(io.BytesIO(final_file))
+            image.save('recieved.jpg')
+            
+            recieved_file_pieces = {}
 
 class ClientThread(threading.Thread):
     def __init__(self,clientAddress,clientsocket):
@@ -37,29 +32,30 @@ class ClientThread(threading.Thread):
         print ("Connection from : ", clientAddress)
         #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
         msg = ''
-        while True:
-            bdata_id = self.csocket.recv(16)
-            data_id = int.from_bytes(bdata_id, 'big')
-            
-            chunk_size = self.csocket.recv(16)
-            print(chunk_size)
-            length = int.from_bytes(chunk_size, 'big')
-            print(f"Chunk of size {length} bytes to be recieved")
-            
-            data = b''
-            while len(data) < length:
-                # doing it in batches is generally better than trying
-                # to do it all in one go, so I believe.
-                to_read = length - len(data)
-                data += self.csocket.recv(4096 if to_read > 4096 else to_read)
+        
+        bdata_id = self.csocket.recv(16)
+        data_id = int.from_bytes(bdata_id, 'big')
+        
+        chunk_size = self.csocket.recv(16)
+        print(chunk_size)
+        length = int.from_bytes(chunk_size, 'big')
+        print(f"Chunk of size {length} bytes to be recieved")
+        
+        data = b''
+        while len(data) < length:
+            # doing it in batches is generally better than trying
+            # to do it all in one go, so I believe.
+            to_read = length - len(data)
+            data += self.csocket.recv(4096 if to_read > 4096 else to_read)
 
-            # send our "done" ack
-            print(f"Chunk number {data_id} of size {len(data)} bytes recieved")
-            recieved_file_pieces[data_id] = data
-            # print(len(recieved_file_pieces))
+        # send our "done" ack
+        print(f"Chunk number {data_id} of size {len(data)} bytes recieved")
+        recieved_file_pieces[data_id] = data
+        print(len(recieved_file_pieces))
 
-            msg = "transmission done"
-            self.csocket.send(msg.encode('utf-8'))
+        msg = "transmission done"
+        self.csocket.send(msg.encode('utf-8'))
+            
 
         print ("Client at ", clientAddress , " disconnected...")
 
@@ -73,7 +69,7 @@ print("Waiting for client request..")
 
 recieved_file_pieces = {}
 
-t1 = threading.Thread(target=merge_img, args=[recieved_file_pieces])
+t1 = threading.Thread(target=merge_img, args=[])
 t1.start()
 
 while True:
